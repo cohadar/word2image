b9f75cfe18fd0feb3c8f53da9a9b63fb40f36578
Igor Hamidovic <igorweg@outlook.com>
2017-12-26 09:46:11 +0100

    init api

--- word2imageApp/word2imageRestApi/word2imageApi.py
+++ word2imageApp/word2imageRestApi/word2imageApi.py
@@ -4,22 +4,10 @@ from flask import request
 
 app = Flask(__name__)
 
-class word2image(object):
-    def __init__(self, word, translate, image1, image2, image3, image4):
-        super(word2image, self).__init__()
-        self.word = word
-        self.translate = translate
-        self.image1 = image1
-        self.image2 = image2
-        self.image3 = image3
-        self.image4 = image4
-
 
 @app.route('/word/<word>', methods=['GET'])
 def get_image_by_word(word):
-    result = word2image(word, 'translate', 'image1', 'image2', 'image3', 'image4')
-    return jsonify({'word': result.word, 'translate': result.translate,
-    'image1': result.image1, 'image2': result.image2, 'image3': result.image3, 'image4': result.image4})
+    return jsonify({'word': word, 'weight': 0, 'text': '', 'imgs': []})
 
 @app.route('/nextUnprocessedWord', methods=['GET'])
 def get_next_unprocessed_word():
