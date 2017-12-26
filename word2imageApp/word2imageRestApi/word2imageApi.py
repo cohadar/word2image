db926f8f4ce8971649474c4b9a2a4c72a57bddaa
Igor Hamidovic <igorweg@outlook.com>
2017-12-26 10:43:11 +0100

    dewerferwf

--- word2imageApp/word2imageRestApi/word2imageApi.py
+++ word2imageApp/word2imageRestApi/word2imageApi.py
@@ -2,6 +2,36 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 import redis
+import sys
+import bs4
+import urllib
+import urllib3
+
+HEADERS = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+SEARCH_URL = "https://www.google.de/search?tbm=isch&q="
+TNB_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:"
+urllib3.disable_warnings()
+http = urllib3.PoolManager()
+
+def extract_url(anchor):
+    url = anchor.get('data-src')
+    if url:
+        return url
+    else:
+        name = anchor.get('name')
+        if name is None:
+            sys.stderr.write('extract_url needs to be updated')
+            sys.exit(2)
+        return TNB_URL + name
+
+
+def get_urls(query):
+    r = http.request('GET', SEARCH_URL + urllib.parse.quote(query), headers=HEADERS)
+    if r.status != 200:
+        sys.stderr.write(r.status)
+        sys.exit(1)
+    soup = bs4.BeautifulSoup(r.data, 'html.parser')
+    return [extract_url(a) for a in soup.find_all("img", attrs={'class': 'rg_ic rg_i'})]
 
 app = Flask(__name__)
 
@@ -24,15 +54,16 @@ def get_image_by_word(word):
         images.append(str(imgs[3].decode('utf-8')))
     return jsonify({'word': word, 'weight': weight, 'text': text, 'imgs': images})
 
-@app.route('/nextUnprocessedWord', methods=['GET'])
-def get_next_unprocessed_word():
-    return jsonify({'nextWord': 'word'})
-
-@app.route('/word2image', methods=['POST'])
+@app.route('/appendimg/<word>', methods=['POST'])
 def insert_word_2_image():
     data = request.get_json()
     result =  word2image(data['word'], data['translate'], data['image1'], data['image2'], data['image3'], data['image4'])
     return jsonify({'result': 'done'})
+
+@app.route('/searchurls/<query>', methods=['GET'])
+def searchImages(query):
+    urls = get_urls(query)
+    return jsonify({'urls': urls})
 
 if __name__ == '__main__':
     app.run(debug=True)
