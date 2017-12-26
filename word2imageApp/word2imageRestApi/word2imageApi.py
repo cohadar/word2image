c7a187b41293b8d2703c0c934adb33aacd0c1de8
Igor Hamidovic <igorweg@outlook.com>
2017-12-26 10:03:36 +0100

    implementation for word get action

--- word2imageApp/word2imageRestApi/word2imageApi.py
+++ word2imageApp/word2imageRestApi/word2imageApi.py
@@ -1,13 +1,28 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+import redis
 
 app = Flask(__name__)
 
+redis_db = redis.StrictRedis(host="iv.hackaton.tda.link", port=6379, db=0, password='svebisekeljubilemornare')
 
 @app.route('/word/<word>', methods=['GET'])
 def get_image_by_word(word):
-    return jsonify({'word': word, 'weight': 0, 'text': '', 'imgs': []})
+    weight = redis_db.get("WORD:" + word)
+    weight.decode('utf-8')
+    weight = int(weight)
+    text = redis_db.get("TEXT:" + word)
+    text.decode('utf-8')
+    text = str(text)
+    imgs = redis_db.get("LRANGE:" + word + "0 3")
+    images = []
+    if(imgs):
+        images.append(str(imgs[0].decode('utf-8')))
+        images.append(str(imgs[1].decode('utf-8')))
+        images.append(str(imgs[2].decode('utf-8')))
+        images.append(str(imgs[3].decode('utf-8')))
+    return jsonify({'word': word, 'weight': weight, 'text': text, 'imgs': images})
 
 @app.route('/nextUnprocessedWord', methods=['GET'])
 def get_next_unprocessed_word():
