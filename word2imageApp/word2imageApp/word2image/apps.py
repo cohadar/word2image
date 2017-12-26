4a2b3881981586c4fc376afeb617e3b8bf27c726
Igor Hamidovic <igorweg@outlook.com>
2017-12-26 09:39:39 +0100

    remove django

--- word2imageApp/word2imageApp/word2image/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Word2ImageConfig(AppConfig):
-    name = 'word2image'
--- word2imageApp/word2imageApp/word2image/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Word2image
-
-
-class word2imageSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Word2image
-        fields = '__all__'
